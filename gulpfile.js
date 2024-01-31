41fe5ec61c4f14f0b5a5811ab4085ca27fe499e8
Vadim Medvedev <vadim130809@gmail.com>
2024-01-31 21:43:28 +0300

    some rewrite logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,44 @@
 import gulp from 'gulp'
+import fileInclude from 'gulp-file-include'
+import nodeSass from 'sass'
+import gulpSass from 'gulp-sass'
+import browserSync from 'browser-sync'
 
-const srcPath = './src/**/*.*'
-const distPath = './dist/'
+const sassHandler = gulpSass(nodeSass)
 
-gulp.task('copy', () => {
-  return gulp.src(srcPath)
-    .pipe(gulp.dest(distPath))
+const paths = {
+  build: './dist',
+  htmlPages: './src/html/pages/*.html',
+  styles: './src/styles/*.scss',
+  buildHtml: './dist/pages',
+  buildStyles: './dist/styles',
+  public: './public/**/*'
+}
+
+const plugins = {
+  fileInclude,
+  sassHandler,
+  browserSync
+}
+
+gulp.task('html-include', () => {
+  return gulp.src(paths.htmlPages)
+    .pipe(plugins.fileInclude())
+    .pipe(gulp.dest(paths.buildHtml))  
 })
 
-const devSeries = gulp.series('copy')
+gulp.task('scss-handler', () => {
+  return gulp.src(paths.styles)
+    .pipe(plugins.sassHandler())
+    .pipe(gulp.dest(paths.buildStyles))
+})
 
-gulp.task('dev', devSeries)
-gulp.task('default', devSeries)
+//gulp.task('start-server', () => {
+//  return plugins.browserSync.init({
+//    server: {
+//      baseDir: paths.build
+//    },
+//    notify: false,
+//    port: 3000 //!!
+//  })
+//})
